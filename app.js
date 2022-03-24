4bb4d8006f7d6b60cfa10be45d559f1f3125dab9
Ross Burns <101517539+rossburns81@users.noreply.github.com>
2022-03-24 16:08:21 -0500

    Completed

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+let end = 100;
+for (let num = 1; num <= end; num ++) {
+    if (num % 2 != 0) {
+        console.log(num)
+    }
+}
 
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
+for (let y = 1; y <= 100; y++) {
+    if (y % 3 == 0 && y % 5 == 0) {
+        console.log(y, "FIZZBUZZ")
+    } else if (y % 3 == 0) {
+        console.log(y,"FIZZ")
+    } else if (y % 5 == 0) {
+        console.log(y, "BUZZ")
+    }
+}
